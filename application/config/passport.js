caf57b7505f830e3dea55e39a10bc22d4eaff810
Luis Elizondo <lelizondo@gmail.com>
2015-03-21 12:30:19 -0600

    Enable passport

--- application/config/passport.js
+++ application/config/passport.js
@@ -28,8 +28,8 @@ module.exports.passport = {
     protocol: 'oauth',
     strategy: require('passport-twitter').Strategy,
     options: {
-      consumerKey: 'your-consumer-key',
-      consumerSecret: 'your-consumer-secret'
+      consumerKey: process.env.TWITTER_APIKEY,
+      consumerSecret: process.env.TWITTER_APISECRET
     }
   },
 
