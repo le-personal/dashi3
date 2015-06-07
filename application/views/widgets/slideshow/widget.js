ea3ec8858478e53fa1f253a5d3d2ed85897c3995
Luis Elizondo <lelizondo@gmail.com>
2015-06-06 23:50:52 -0500

    Fix #9 Add widget slideshow

---
+++ application/views/widgets/slideshow/widget.js
@@ -0,0 +1,131 @@
+(function() {
+	'use strict';
+
+	angular.module("dashi3")
+	.controller("WidgetSlideshowController", [
+		"$scope",
+		"$rootScope",
+		"$modal",
+		"$sails",
+		function($scope, $rootScope, $modal, $sails) {
+			var widget = $scope.widget;
+
+			$scope.slides = [];
+
+			// When opening the widget we need to get all the latest data
+			// this will also subscribe ourself to the data model so we
+			// can listen to changes
+			io.socket.get("/api/v1/data/" + widget.dataset, function(data) {
+				angular.forEach(data, function(item) {
+					$scope.slides.push(item.content);
+				});
+			});
+
+			// when there is a change on the server, update
+			// data is refering to the model Data
+			io.socket.on("data", function(data) {
+				if(data.verb == "created") {
+					if(data.data.dataset == widget.dataset) {
+						$scope.slides.push(data.data.content);
+					}
+				}
+			});
+
+			/**
+			 * React to event "openAddDataPoint"
+			 * @type {[type]}
+			 */
+			$scope.openForm = function(widget) {
+				$modal.open({
+					templateUrl: "/widgets/slideshow/form",
+					controller: "WidgetSlideshowFormController",
+					resolve: {
+						widget: function() {
+							return $scope.widget;
+						}
+					}
+				});
+			}
+
+			/**
+			 * React to event "openWidgetSettings"
+			 * @type {[type]}
+			 */
+			$scope.openSettings = function(widget) {
+				$modal.open({
+					templateUrl: "/widgets/slideshow/settings",
+					controller: "WidgetSlideshowSettingsController",
+					resolve: {
+						widget: function() {
+							return $scope.widget;
+						}
+					}
+				});
+			}
+		}
+	])
+
+	.controller("WidgetSlideshowSettingsController", [
+		"$scope",
+		"$rootScope",
+		"$modalInstance",
+		"widget",
+		"Widgets",
+		function($scope, $rootScope, $modalInstance, widget, Widgets) {
+			$scope.data = widget;
+			$scope.remove = function() {
+				Widgets.remove({widgetId: widget.id});
+				$rootScope.$broadcast("dashboard:widget:remove", widget);
+				$modalInstance.close();
+			}
+
+			$scope.update = function () {
+				Widgets.update({widgetId: widget.id}, {
+					title: $scope.data.title,
+					description: $scope.data.description,
+					dataset: $scope.data.dataset,
+					settings: {
+						
+					}
+				}, function(result) {
+					console.log(result);
+				});
+
+		    $modalInstance.close();
+		  };
+
+		  $scope.cancel = function () {
+		    $modalInstance.dismiss('cancel');
+		  }
+		}
+	])
+
+	.controller("WidgetSlideshowFormController", [
+		"$scope",
+		"$modalInstance",
+		"widget",
+		"Data",
+		function($scope, $modalInstance, widget, Data) {
+			$scope.ok = function () {
+				var data = {
+					widgetId: widget.id,
+					value: $scope.value
+				}
+
+				// By using the API, we make sure that the event is received
+				// via sockets, if we use sockets to save the data, the event
+				// will not be received for some reason
+				Data.save(data, function(result) {
+					console.log(result);
+		    	$modalInstance.close();
+				});
+		  };
+
+		  $scope.cancel = function () {
+		    $modalInstance.dismiss('cancel');
+		  }
+		}
+	]);
+
+
+})();
