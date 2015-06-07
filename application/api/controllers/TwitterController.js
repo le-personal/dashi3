48a821ecd7e55fd7ac9ee5f1f521c33ce6e7105d
Luis Elizondo <lelizondo@gmail.com>
2015-06-07 15:56:23 -0500

    Adapt TwitterController to use global configuration

--- application/api/controllers/TwitterController.js
+++ application/api/controllers/TwitterController.js
@@ -5,38 +5,40 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 var Twit = require('twit')
- 
 
 module.exports = {
 	stream: function(req, res){
     var term = req.param('term');
-    var language = req.param("language");
+    var language = req.param("language") ? req.param("language") : "en";
     if (term && req.isSocket) {
-    	Passport.findOne({user: req.session.passport.user, provider: "twitter"}).
-    	exec(function(err, data) {
-    		if(err) console.log(err);
-    		if(data) {
-    			if(data.tokens.token && data.tokens.tokenSecret) {
-						var T = new Twit({
-						  consumer_key: process.env.TWITTER_APIKEY,
-						  consumer_secret: process.env.TWITTER_APISECRET,
-						  access_token: data.tokens.token,
-						  access_token_secret: data.tokens.tokenSecret
-						});
 
-						console.log("looking for tweets with the word: " + term + " using the language " + language);
-			    	var stream = T.stream("statuses/filter", {track: term, language: language});
+    	console.log("Term detected: " + term);
+    	if(req.isSocket) console.log("Is socket");
 
-			    	stream.on("tweet", function(tweet) {
-			    		sails.sockets.emit(sails.sockets.id(req.socket), "twitter:stream:" + term.replace(" ", "_"), {tweet: tweet});
-			    	});
+    	var data = sails.config.settings;
+    	
+  		console.log("Getting configuration data");
+  		console.log(data);
 
-			    	sails.io.on("disconnect", function(socket) {
-			    		stream.stop();
-			    	});
-    			}
-    		}
-    	});
+			if(data.twitter_token && data.twitter_token_secret) {
+				var T = new Twit({
+				  consumer_key: process.env.TWITTER_APIKEY,
+				  consumer_secret: process.env.TWITTER_APISECRET,
+				  access_token: data.twitter_token,
+				  access_token_secret: data.twitter_token_secret
+				});
+
+				console.log("looking for tweets with the word: " + term + " using the language " + language);
+	    	var stream = T.stream("statuses/filter", {track: term, language: language});
+
+	    	stream.on("tweet", function(tweet) {
+	    		sails.sockets.emit(sails.sockets.id(req.socket), "twitter:stream:" + term.replace(" ", "_"), {tweet: tweet});
+	    	});
+
+	    	sails.io.on("disconnect", function(socket) {
+	    		stream.stop();
+	    	});
+			}
     }
 	},
 
@@ -47,38 +49,37 @@ module.exports = {
 	username: function(req, res) {
 		var term = req.param('username');
     var counter = 0;
+		
     if (term && req.isSocket) {
-    	Passport.findOne({user: req.session.passport.user, provider: "twitter"}).
-    	exec(function(err, data) {
-    		if(err) console.log(err);
-    		if(data) {
-    			if(data.tokens.token && data.tokens.tokenSecret) {
-						var T = new Twit({
-						  consumer_key: process.env.TWITTER_APIKEY,
-						  consumer_secret: process.env.TWITTER_APISECRET,
-						  access_token: data.tokens.token,
-						  access_token_secret: data.tokens.tokenSecret
-						});
-						
-						console.log("Looking for tweets of the username: " + username);
-			    	var stream = T.stream("user");
+    	config.all()
+    	.then(function(data) {
+				if(data.twitter_token && data.twitter_token_secret) {
+					var T = new Twit({
+					  consumer_key: process.env.TWITTER_APIKEY,
+					  consumer_secret: process.env.TWITTER_APISECRET,
+					  access_token: data.twitter_token,
+					  access_token_secret: data.twitter_token_secret
+					});
+					
+					console.log("Looking for tweets of the username: " + username);
+		    	var stream = T.stream("user");
 
-			    	stream.on("tweet", function(tweet) {
-			    		counter++;
-			    		sails.sockets.emit(sails.sockets.id(req.socket), "twitter:username:" + term.replace(" ", "_"), {tweet: tweet});
+		    	stream.on("tweet", function(tweet) {
+		    		counter++;
+		    		sails.sockets.emit(sails.sockets.id(req.socket), "twitter:username:" + term.replace(" ", "_"), {tweet: tweet});
 
-			    		if(counter > 10) {
-			    			console.log("Found 10 tweets");
-			    			counter = 0;
-			    		}
-			    	});
+		    		if(counter > 10) {
+		    			console.log("Found 10 tweets");
+		    			counter = 0;
+		    		}
+		    	});
 
-			    	sails.io.on("disconnect", function(socket) {
-			    		stream.stop();
-			    	});
-    			}
-    		}
+		    	sails.io.on("disconnect", function(socket) {
+		    		stream.stop();
+		    	});
+				}
     	});
+
     }
 	}
 
